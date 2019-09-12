ec313a6f3c5166528f443b08521d9688a3978f1a
catsoousa <53863430+catsoousa@users.noreply.github.com>
2019-09-12 10:47:58 -0300

    Update jogovelha.py

--- src/jogovelha.py
+++ src/jogovelha.py
@@ -1,22 +1,14 @@
-import jogovelha
-import sys
+def inicializar():
+	tab = []
+	for i in range(3):
+		linha = []
+		for j in range(3):
+			linha.append(".")
+		tab.append(linha)
+	return tab
 
-erroInicializar = False
-
-
-jogo = jogovelha.inicializar()
-
-if len(jogo) != 3:
-	erroInicializar = True
-else:
-	for linha in jogo:
-		if len(linha) != 3:
-			erroInicializar = True
-		else:
-			for elemento in linha:
-				if elemento != '.':
-					erroInicializar = True
-if erroInicializar:
-	sys.exit(1)
-else:
-	sys.exit(0)
+def main():
+	jogo = inicializar()
+	print(jogo)
+if __name__ == "__main__":
+	main()
